2fbeb027ce8582f08e484bc3c4ef5c5717ed4cb8
tccSolutions <tim@tccs.tech>
2021-12-13 07:05:30 -0500

    updated rout("/") main.py

--- main.py
+++ main.py
@@ -2,17 +2,12 @@ import gunicorn
 import psycopg2
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template('index.html')
-
-
-
-
 
 
 if __name__ == "__main__":
