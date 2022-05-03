6dfba0533919fe10e20a38d3cd675e8f7ab9fa53
tccSolutions <tim@tccs.tech>
2022-05-03 07:20:57 -0400

    update to contact page response

--- main.py
+++ main.py
@@ -39,7 +39,9 @@ def contact_me():
     if form.validate_on_submit():
         msg = Message("Please Help Message", sender="tim@tccs.tech",
                       recipients=["tim@tccs.tech"])
-        msg.body = request.form["message"]        
+        sender = request.form["email"]
+        message = request.form["message"]
+        msg.body = f"From:{sender}\n\n{message}"
         mail.send(msg)      
         return render_template('contact_page/contact_good.html', name=form.name.data, email=form.email.data)
     return render_template('/contact_page/contact.html', form=form)
